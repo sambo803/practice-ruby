5f314aeac66d2074af030f8c08ac9c5228089fbc
samuel <sbacon803@gmail.com>
2022-07-19 14:04:24 -0600

    add class items

--- store_item.rb
+++ store_item.rb
@@ -1,14 +1,39 @@
-#drug store
-product1 = {
-  :name => "tylenol",
-  :price => 3,
-  :color => "white",
-}
-p product1
+# #drug store
+# product1 = {
+#   :name => "tylenol",
+#   :price => 3,
+#   :color => "white",
+# }
+# p product1
 
-product2 = {
-  :name => "beer",
-  :price => 8,
-  :color => "amber",
-}
-p product2
+# product2 = {
+#   :name => "beer",
+#   :price => 8,
+#   :color => "amber",
+# }
+# p product2
+
+class Product
+  def initialize(input_name, input_price, input_color)
+    @name = input_name
+    @price = input_price
+    @color = input_color
+  end
+
+  def name
+    @name
+  end
+
+  def price
+    @price
+  end
+
+  def color
+    @color
+  end
+end
+
+prod1 = Product.new("tylenol", 3, "white")
+p prod1
+prod2 = Product.new("beer", 8, "amber")
+p prod2
